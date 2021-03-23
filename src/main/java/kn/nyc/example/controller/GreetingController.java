b6aff7898a8516d243c74fdc9d41ce5346824921
ruengdej.boonphadung <ruengdej.boonphadung@Kuehne-Nagel.com>
2021-03-23 17:20:43 -0400

    initial commit

---
+++ src/main/java/kn/nyc/example/controller/GreetingController.java
@@ -0,0 +1,13 @@
+package kn.nyc.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/kn/nyc/example/SpringBootHelloWorldOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package kn.nyc.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
