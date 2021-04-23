cd838e3974aa4e27d0fe8c628496984abdb1b23d
ruengdej.boonphadung <ruengdej.boonphadung@Kuehne-Nagel.com>
2021-04-23 19:27:24 -0400

    add AppName

--- src/main/java/kn/nyc/example/controller/GreetingController.java
+++ src/main/java/kn/nyc/example/controller/GreetingController.java
@@ -1,13 +1,19 @@
 package kn.nyc.example.controller;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
+	@Value("${thisapp.name:'DefaultName'}")
+	private String appName; 
+	
 	@GetMapping("/")
 	public String hello() {
-		return "Hello World!";
+		return "Hello World! from: '"+appName+"' The time is "+new Date();
 	}
 }
